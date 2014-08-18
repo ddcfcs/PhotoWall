d73eb8526de6d4ce0af4551b47f7d91a64c70abf
Cruise <mac@macdeMacBook-Air.local>
2014-08-18 20:41:30 +0800

    Initial Commit

---
+++ PhotoWall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhotoWall
+//
+//  Created by Cruise on 14-8-18.
+//  Copyright (c) 2014年 Ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
